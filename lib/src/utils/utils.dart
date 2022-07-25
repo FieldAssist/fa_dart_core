7ba3db5839a2be4ea952a787f5bff5ce5446ad48
Sameer Kumar <sameerkumar.tman@gmail.com>
2022-07-25 18:55:20 +0530

    [Utils] Create function formatValue

--- lib/src/utils/utils.dart
+++ lib/src/utils/utils.dart
@@ -15,3 +15,28 @@ bool ifContainsKeyAndNotEmpty<T>(Map<String, T> map, String key) {
 bool isStringEmpty(String? value) => !checkIfNotEmpty(value);
 
 bool isListEmpty(List? value) => !checkIfListIsNotEmpty(value);
+
+/// Converts passed double in K, L and Cr notion, significantly shrinking the length of given number.
+/// For example:
+///   Any double less than 10000 will be returned as it is depending on bool isReturnTypeDouble
+///   34394941.23 is not easily readable, now it will returns this value as 3.44Cr
+///   32421.32 will be returned as 32.42K
+///   324936.21 will be returned as 3.25L
+String formatValue(double value, {bool isReturnTypeDouble = false}) {
+  if (value > 10000000) {
+    final calculated = value / 10000000;
+    return '${calculated.toStringAsFixed(2)}Cr';
+  } else if (value > 100000) {
+    final calculated = value / 100000;
+    return '${calculated.toStringAsFixed(2)}L';
+  } else if (value > 10000) {
+    final calculated = value / 1000;
+    return '${calculated.toStringAsFixed(2)}K';
+  } else {
+    if (isReturnTypeDouble) {
+      return value.toStringAsFixed(2);
+    } else {
+      return value.toInt().toString();
+    }
+  }
+}
