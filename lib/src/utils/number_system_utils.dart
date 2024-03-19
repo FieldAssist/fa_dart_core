78eb220cc22a307dc86800636112fbefbd9005a2
Khushboo Agrawal <113587610+Khushboo1702@users.noreply.github.com>
2024-03-19 14:08:35 +0530

    feat(utils): implementation of number system (#15)
    * feat(utils): implementation of number system
    
    * fix(number-system): changing name of the file
    
    * fix(utils): formatting code
    
    * fix(number-system): removing unnecessary variable

---
+++ lib/src/utils/number_system_utils.dart
@@ -0,0 +1,25 @@
+import 'package:fa_dart_core/fa_dart_core.dart';
+
+class NumberSystemUtils {
+  String parse(bool usesInternationalNumberSystem, num? val,
+          {int decimalPlaces = 0}) =>
+      usesInternationalNumberSystem
+          ? _parseAsInternational(val ?? 0, decimalPlaces: decimalPlaces)
+          : _parseAsIndian(val ?? 0, decimalPlaces: decimalPlaces);
+
+  String _parseAsInternational(num val, {int decimalPlaces = 0}) {
+    return formatValue(
+      val.toDouble(),
+      locale: 'en_US',
+      decimalPlaces: decimalPlaces,
+    );
+  }
+
+  String _parseAsIndian(num val, {int decimalPlaces = 0}) {
+    return formatValue(
+      val.toDouble(),
+      locale: 'en_IN',
+      decimalPlaces: decimalPlaces,
+    );
+  }
+}
